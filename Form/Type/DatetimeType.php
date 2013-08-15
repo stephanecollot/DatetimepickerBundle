45764477ce818715a82457e158e0cafcb7a84628
Stephane Collot <stephane.collot@gmail.com>
2013-08-15 23:41:03 +0200

    Symfony date format meet JavaScript date format

--- Form/Type/DatetimeType.php
+++ Form/Type/DatetimeType.php
@@ -34,6 +34,7 @@ class DatetimeType extends AbstractType
     public function __construct(array $options)
     {
         $this->options = $options;
+        
     }
     
     /**
@@ -41,13 +42,19 @@ class DatetimeType extends AbstractType
     */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
+        
         $pickerOptions = $options['pickerOptions'];
 
+        //Set automatically the language
         if(!isset($options['pickerOptions']['language']))
             $pickerOptions['language'] = \Locale::getDefault();
-        
         if($pickerOptions['language'] == 'en')
             unset($pickerOptions['language']);
+        
+        //Set the defaut format of malot.fr/bootstrap-datetimepicker
+        if(!isset($options['pickerOptions']['format']))
+            $pickerOptions['format'] = 'mm/dd/yyyy';
+
 
         $view->vars = array_replace($view->vars, array(
             'pickerOptions' => $pickerOptions,
@@ -59,13 +66,100 @@ class DatetimeType extends AbstractType
     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
+        $configs = $this->options;
+        
         $resolver
             ->setDefaults(array(
                 'widget' => 'single_text',
+                'format' => function (Options $options, $value) use ($configs) {
+                    if(isset($options['pickerOptions']['format']))
+                        return $this->convertMalotToIntlFormater( $options['pickerOptions']['format'] );
+                    else
+                        return $this->convertMalotToIntlFormater( 'mm/dd/yyyy' );
+                },
                 'pickerOptions' => array(),
             ));
     }
 
+    /**
+    * 
+    */
+    public function convertMalotToIntlFormater($formatter)
+    {
+        $malotFormater  =  array("yyyy", "ss", "ii", "hh", "HH", "dd", "mm", "MM",   "yy");
+        $intlFormater   =  array("yyyy", "ss", "mm", "HH", "hh", "dd", "MM", "MMMM", "yy");
+        $return = str_replace($malotFormater, $intlFormater, $formatter);
+        
+        $malotFormater  =  array("p", "P", "s", "i", "h", "H", "d", "m", "M");
+        $intlFormater   =  array("a", "a", "s", "m", "H", "h", "d", "M", "MMM");
+        $return = str_replace($malotFormater, $intlFormater, $return);
+        
+        
+        $patterns = preg_split('([\\\/.:_;,\s-\ ]{1})', $formatter);
+        $exits = array();
+
+        // Transform pattern for JQuery ui datepicker
+        foreach ($patterns as $index => $val) {
+            switch ($val) {
+                case 'yyyy':
+                    $exits[$val] = 'yyyy';
+                    break;
+                case 'ss':
+                    $exits[$val] = 'ss';
+                    break;
+                case 'ii':
+                    $exits[$val] = 'mm';
+                    break;
+                case 'hh':
+                    $exits[$val] = 'HH';
+                    break;
+                case 'HH':
+                    $exits[$val] = 'hh';
+                    break;
+                case 'dd':
+                    $exits[$val] = 'dd';
+                    break;
+                case 'mm':
+                    $exits[$val] = 'MM';
+                    break;
+                case 'MM':
+                    $exits[$val] = 'MMMM';
+                    break;
+                case 'p':
+                case 'P':
+                    $exits[$val] = 'a';
+                    break;
+                case 's':
+                    $exits[$val] = 's';
+                    break;
+                case 'i':
+                    $exits[$val] = 'm';
+                    break;
+                case 'h':
+                    $exits[$val] = 'H';
+                    break;
+                case 'H':
+                    $exits[$val] = 'h';
+                    break;
+                case 'd':
+                    $exits[$val] = 'd';
+                    break;
+                case 'm':
+                    $exits[$val] = 'M';
+                    break;
+                case 'M':
+                    $exits[$val] = 'MMM';
+                    break;
+                
+            }
+        }
+
+        return str_replace(array_keys($exits), array_values($exits), $formatter);
+        
+    }
+
+    
+    
     /**
     * {@inheritdoc}
     */
@@ -73,7 +167,7 @@ class DatetimeType extends AbstractType
     {
         return 'datetime';
     }
-
+    
     /**
     * {@inheritdoc}
     */
