ceb52b7e0ea69236ad13534811dc9af2a1229b1f
Stephane Collot <stephane.collot@gmail.com>
2013-08-16 01:40:31 +0200

    clean up delete unused files

--- Tests/Functional/TestBundle/TestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SC\DatetimepickerBundle\StephaneCollotDatetimepickerBundle\Tests\Functional\TestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TestBundle extends Bundle
-{
-}
